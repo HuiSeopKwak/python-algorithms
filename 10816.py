ab52fca83080889b71e12e2b9cff578e80dc334f
HuiSeopKwak <kh06033@naver.com>
2024-01-18 20:03:20 +0900

    0118

--- 10816.py
+++ 10816.py
@@ -3,9 +3,15 @@ li = list(map(int, input().split()))
 M = int(input())
 li2 = list(map(int, input().split()))
 
+dic = {}
+for i in li:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
+
 for i in li2:
-    cnt = 0
-    for j in li:
-        if i == j:
-            cnt += 1
-    print(cnt, end=' ')
+    if dic.get(i) == None:
+        print(0, end=' ')
+    else:
+        print(dic.get(i), end=' ')
