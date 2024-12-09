e56d895579b7483be7b0e7c7f8144f113848094b
HuiSeopKwak <kh06033@naver.com>
2024-12-09 10:10:32 +0900

    add 11659.py

--- 11659.py
+++ 11659.py
@@ -1,10 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split()) # n 숫자 수 m 합 구해야 하는 수
 nums = list(map(int, input().split()))
 nums.insert(0, 0)
 dp = [0 for _ in range(n+1)]
+
 for i in range(1, n+1):
     dp[i] = dp[i-1] + nums[i]
-print(dp)
+
 for _ in range(m):
     a, b = map(int, input().split()) # a, b 구간
-    print('====', dp[b] - dp[a-1])
+    print(dp[b] - dp[a-1])
